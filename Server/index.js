c7da3d53eacb5352c2a63f28b053a57b01c9d8d0
Michael Tombor <tombormt@gmail.com>
2018-05-15 06:22:51 +0000

    add passport

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,9 @@
 const express = require ('express');
+const passport = require ('passport');
+const GoogleStrategy = require('passport-google=oauth20').Strategy;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there'});
-});
+
 
 const PORT = process.env.PORT;
 app.listen(PORT);
