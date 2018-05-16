62e7551ee03b6ac40b8f5426360f5131e8a1d70a
Michael Tombor <tombormt@gmail.com>
2018-05-16 21:48:54 +0000

    add keys file

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,12 @@
 const express = require ('express');
 const passport = require ('passport');
-const GoogleStrategy = require('passport-google=oauth20').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
+//clientID: 75742664433-a6ssa1v5k91gcsujrsavv7ljhdp75kg1.apps.googleusercontent.com
+//clientSecret: 5DhitzUeyEEex5jOiOl230xh
 
+//passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT;
 app.listen(PORT);
