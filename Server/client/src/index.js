aa25b449782904d6b2a7bf2f41e69aec56b90171
Michael Tombor <tombormt@gmail.com>
2018-06-07 22:34:03 +0000

    add render component to index.js

--- Server/client/src/index.js
+++ Server/client/src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 //takes two arguments. Root component & where we are rendering component inside of DOM
-ReactDOM.render(<App />, );
+ReactDOM.render(<App />, document.querySelector('#root'));
