3f3bfbbbccd6de84a84c2549680593e97effacbd
Michael Tombor <tombormt@gmail.com>
2018-06-07 22:29:48 +0000

    add App.js and render App.js inside index.js

--- Server/client/src/index.js
+++ Server/client/src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 //takes two arguments. Root component & where we are rendering component inside of DOM
-ReactDOM.render();
+ReactDOM.render(<App />, );
