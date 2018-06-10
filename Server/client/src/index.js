11de71af1751257731741a9a7cb5527efae72b44
Michael Tombor <tombormt@gmail.com>
2018-06-10 05:57:49 +0000

    set up redux store and hooked up to react with Provider

--- Server/client/src/index.js
+++ Server/client/src/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/App';
+
+//assign a reducer to the store. 
+//The first argument is an array, 
+//Second argument is the initial state
+//Third is the applyMiddleware 
+const store = createStore(() => [], {}, applyMiddleware());
 
 //takes two arguments. Root component & where we are rendering component inside of DOM
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+    <Provider store={store}><App /></Provider>,
+    document.querySelector('#root')
+);
