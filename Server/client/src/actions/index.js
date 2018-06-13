4bd0a0e2586e6010b019e060c9ab4d1c6f894d28
Michael Tombor <tombormt@gmail.com>
2018-06-13 21:13:27 +0000

    update dev.js

--- Server/client/src/actions/index.js
+++ Server/client/src/actions/index.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-const fetchUser = () => {
+export const fetchUser = () => {
   return function(dispatch) {
     //replace with relative path /api/user
-    axios.get('https://node-react-fullstack-michaeltombor.c9users.io:8081/api/user')
+    axios.get('https: //node-react-fullstack-michaeltombor.c9users.io:8081/api/user')
       .then(res => dispatch({ type: FETCH_USER, payload: res }))
   }
 };
