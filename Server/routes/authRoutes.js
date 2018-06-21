a00dceddb16078e9b29b035a9f07ffc4a085a289
Michael Tombor <michaeltombor@Michaels-MacBook-Pro.local>
2018-06-21 14:42:39 -0700

    update authRoutes

--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -1,31 +1,27 @@
-import { S_IRWXG } from 'constants';
+const passport = require("passport");
 
-const passport = require('passport');
-
-module.exports = (app) => {
- app.get(
-      '/auth/google', 
-      passport.authenticate('google', {
-          scope: ['profile', 'email']
-      })
-  );
-  
+module.exports = app => {
   app.get(
-    '/auth/google/callback', 
-    passport.authenticate('google'),
-    (req, res () => {
-        res.redirect('/surveys');
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"]
     })
-);
-  
-  app.get('/api/logout', (req, res) => {
-   req.logout();
-   res.send(req.user);
+  );
+
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
+      res.redirect("/surveys");
+    }
+  );
+
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.send(req.user);
   });
-  
- app.get('/api/user', (req, res) => {
- res.send(req.user);
-});
 
+  app.get("/api/user", (req, res) => {
+    res.send(req.user);
+  });
 };
-
